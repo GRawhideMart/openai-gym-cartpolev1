915bbab263fc7767b3faa8323954679af84864d3
Giulio Martena <giuliomariomartena@gmail.com>
2022-12-31 17:15:35 +0100

    Added policy and target networks to environment

--- environment.py
+++ environment.py
@@ -1,10 +1,23 @@
 import gymnasium as gym
+import torch.optim as optim
 
 from device import standard as device
+from memory import ReplayMemory
+from model import DQN
+from utils.hyperparameters import LR
 
 def launch():
     env = gym.make('CartPole-v1', render_mode = 'human')
+    n_actions = env.action_space.n
     state, info = env.reset()
+    n_obs = len(state)
+
+    policy_net = DQN(n_observations=n_obs, n_actions=n_actions).to(device)
+    target_net = DQN(n_observations=n_obs, n_actions=n_actions).to(device)
+    target_net.load_state_dict(policy_net.state_dict())
+
+    optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
+    memory = ReplayMemory(10000)
 
     for _ in range(1000):
         action = env.action_space.sample()
