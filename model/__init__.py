edeab6e1979d6aca3c6f3989f2e86e6c51d189c1
Giulio Martena <giuliomariomartena@gmail.com>
2023-01-01 13:21:12 +0100

    Fixed a bug in the forward method

--- model/__init__.py
+++ model/__init__.py
@@ -5,11 +5,11 @@ import torch.nn.functional as F
 class DQN(nn.Module):
     def __init__(self, n_observations, n_actions):
         super(DQN, self).__init__()
-        self.input_layer = nn.Linear(n_observations, 256)
-        self.hidden_layer = nn.Linear(256, 256)
-        self.output_layer = nn.Linear(256, n_actions)
+        self.input_layer = nn.Linear(n_observations, 512)
+        self.hidden_layer = nn.Linear(512, 512)
+        self.output_layer = nn.Linear(512, n_actions)
 
     def forward(self, x):
-        x = F.relu(self.input_layer)
-        x = F.relu(x)
+        x = F.leaky_relu(self.input_layer(x), 0.1)
+        x = F.leaky_relu(self.hidden_layer(x), 0.1)
         return self.output_layer(x)
